953f3edd4148693eb14702d2ded98da32c4a14be
Fascinate <eddo92@yahoo.it>
2024-11-19 16:35:14 +0100

    Add files via upload

---
+++ menu avanti e indietro.cs
@@ -0,0 +1,45 @@
+bool continuareCiclo = true;
+int stato = 0;
+
+while (continuareCiclo)
+{
+    if (stato == 0)
+    {
+        introduzione();
+        var scelta = int.Parse(Console.ReadLine());
+        if (scelta == 1)
+        {
+            stato = 1; // Passa al secondo menu
+        }
+        else
+        {
+            Console.WriteLine("Opzione non valida. Riprovare.");
+        }
+    }
+    else if (stato == 1)
+    {
+        Console.WriteLine("premere 1 per andare avanti 2 per tornare indietro");
+        var secondaScelta = int.Parse(Console.ReadLine());
+        if (secondaScelta == 1)
+        {
+            Console.WriteLine("sei andato avanti");
+            continuareCiclo = false; // Esci dal ciclo principale
+        }
+        else if (secondaScelta == 2)
+        {
+            Console.WriteLine("Stai tornando indietro.");
+            stato = 0; // Torna al primo menu
+        }
+        else
+        {
+            Console.WriteLine("Opzione non valida. Riprovare.");
+        }
+    }
+}
+
+Console.ReadKey();
+
+void introduzione()
+{
+    Console.WriteLine("premere 1 per accedere al menu principale");
+}
